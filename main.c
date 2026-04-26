05013d78a370d8d58948a2bb5fbf66e1de1aa0b4
anandanshul <anshulanand194@gmail.com>
2026-04-26 07:49:41 +0000

    add stack, vm

--- main.c
+++ main.c
@@ -1,37 +1,30 @@
 #include "bootlib.h"
 #include "munit.h"
-#include "snekobject.h"
+#include "vm.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-munit_case(RUN, correctly_free, {
-  snek_object_t *first = new_snek_array(1);
-  snek_object_t *second = new_snek_array(1);
-  // refcounts: first = 1, second = 1
-  snek_array_set(first, 0, second);
-  // refcounts: first = 1, second = 2
-  snek_array_set(second, 0, first);
-  // refcounts: first = 2, second = 2
-  refcount_dec(first);
-  refcount_dec(second);
-  assert_int(first->refcount, ==, 1, "Refcount first should be 1");
-  assert_int(second->refcount, ==, 1, "Refcount second should be 1");
+munit_case(RUN, test_vm_new, {
+  vm_t *vm = vm_new();
+  assert_int(vm->frames->capacity, ==, 8, "frames should have capacity 8");
+  assert_int(vm->objects->capacity, ==, 8, "objects should have capacity 8");
+  vm_free(vm);
 });
 
-// Don't touch below this line
+munit_case(SUBMIT, test_vm_new_free, {
+  vm_t *vm = vm_new();
+  vm_free(vm);
+  assert(boot_all_freed());
+});
 
 int main() {
   MunitTest tests[] = {
-      munit_test("/correctly_free", correctly_free),
+      munit_test("/vm", test_vm_new),
+      munit_test("/vm", test_vm_new_free),
       munit_null_test,
   };
 
-  MunitSuite suite = munit_suite("refcount", tests);
+  MunitSuite suite = munit_suite("mark-and-sweep", tests);
 
-  int result = munit_suite_main(&suite, NULL, 0, NULL);
-
-  printf("*** NOTE: A memory leak warning is EXPECTED here ***\n");
-  printf("*** We'll fix the circular reference problem soon ***\n");
-
-  return result;
+  return munit_suite_main(&suite, NULL, 0, NULL);
 }
