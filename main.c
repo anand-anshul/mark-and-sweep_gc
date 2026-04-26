b95abe0bfee28418fdb1b5a5c06badfc83f9ff62
anandanshul <anshulanand194@gmail.com>
2026-04-26 08:32:51 +0000

    add stack frames

--- main.c
+++ main.c
@@ -6,21 +6,37 @@
 
 munit_case(RUN, test_vm_new, {
   vm_t *vm = vm_new();
-  assert_int(vm->frames->capacity, ==, 8, "frames should have capacity 8");
-  assert_int(vm->objects->capacity, ==, 8, "objects should have capacity 8");
+  vm_new_frame(vm);
+  assert_int(vm->frames->count, ==, 1, "frame was pushed");
   vm_free(vm);
 });
 
-munit_case(SUBMIT, test_vm_new_free, {
+munit_case(RUN, test_vm_new_frame, {
   vm_t *vm = vm_new();
+  frame_t *frame = vm_new_frame(vm);
+  assert_ptr(frame->references, !=, NULL,
+             "frame->references must be allocated");
+  assert_int(frame->references->count, ==, 0,
+             "references stack should start empty");
+  assert(frame->references->capacity >
+         0); // references stack must have capacity > 0
+  assert_ptr(frame->references->data, !=, NULL,
+             "references stack backing array must be allocated");
+  vm_free(vm);
+});
+
+munit_case(RUN, test_frames_are_freed, {
+  vm_t *vm = vm_new();
+  vm_new_frame(vm);
   vm_free(vm);
   assert(boot_all_freed());
 });
 
 int main() {
   MunitTest tests[] = {
-      munit_test("/vm", test_vm_new),
-      munit_test("/vm", test_vm_new_free),
+      munit_test("/test_vm_new", test_vm_new),
+      munit_test("/test_vm_new_frame", test_vm_new_frame),
+      munit_test("/test_frames_are_freed", test_frames_are_freed),
       munit_null_test,
   };
 
--- vm.c
+++ vm.c
@@ -1,5 +1,22 @@
 #include "vm.h"
 
+void vm_frame_push(vm_t *vm, frame_t *frame) { stack_push(vm->frames, frame); }
+
+frame_t *vm_new_frame(vm_t *vm) {
+  frame_t *frame = malloc(sizeof(frame_t));
+  frame->references = stack_new(8);
+
+  vm_frame_push(vm, frame);
+  return frame;
+}
+
+void frame_free(frame_t *frame) {
+  stack_free(frame->references);
+  free(frame);
+}
+
+// don't touch below this line
+
 vm_t *vm_new() {
   vm_t *vm = malloc(sizeof(vm_t));
   if (vm == NULL) {
@@ -7,28 +24,15 @@ vm_t *vm_new() {
   }
 
   vm->frames = stack_new(8);
-  if (vm->frames == NULL) {
-    free(vm);
-    return NULL;
-  }
-
   vm->objects = stack_new(8);
-  if (vm->objects == NULL) {
-    stack_free(vm->frames);
-    free(vm);
-    return NULL;
-  }
-
   return vm;
 }
 
 void vm_free(vm_t *vm) {
-  if (vm == NULL) {
-    return;
+  for (int i = 0; i < vm->frames->count; i++) {
+    frame_free(vm->frames->data[i]);
   }
-
   stack_free(vm->frames);
   stack_free(vm->objects);
-
   free(vm);
 }
